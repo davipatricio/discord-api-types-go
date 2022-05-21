030e9d17a531ece57c05db053dc0688473c9e2e3
davipatricio <davipatricio2006@gmail.com>
2022-05-21 10:04:55 -0300

    Create Gateway/Generic types

---
+++ gateway/generic.go
@@ -0,0 +1,10 @@
+package types
+
+type GatewayURLQuery struct {
+	// The gateway version to use
+	V string `json:"v"`
+	// The encoding to use. Can be either "json" or "etf"
+	Encoding string `json:"encoding"`
+	// Which compression to use. Can empty or "zlib-stream"
+	Compress string `json:"compress"`
+}
