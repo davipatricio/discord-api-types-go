282bdb0b5831ae6a7510e91f8d6e3c7145eb89aa
davipatricio <davipatricio2006@gmail.com>
2022-05-28 22:35:21 -0300

    gateway: replace vars with constants and add types

--- gateway/gateway.go
+++ gateway/gateway.go
@@ -5,148 +5,148 @@ const GatewayVersion string = "10"
 // https://discord.com/developers/docs/topics/opcodes-and-status-codes#gateway-gateway-opcodes
 const (
 	// A event was dispatched
-	GatewayOpcodesDispatch = 0
+	GatewayOpcodesDispatch int = 0
 	// A heartbeat was received
-	GatewayOpcodesHeartbeat = 1
+	GatewayOpcodesHeartbeat int = 1
 	// Starts a new session during the initial handshake
-	GatewayOpcodesIdentify = 2
+	GatewayOpcodesIdentify int = 2
 	// Update the client's presence
-	GatewayOpcodesPresenceUpdate = 3
+	GatewayOpcodesPresenceUpdate int = 3
 	// Used to join/leave or move between voice channels
-	GatewayOpcodesVoiceStateUpdate = 4
+	GatewayOpcodesVoiceStateUpdate int = 4
 	// Resume a previous session that was disconnected
-	GatewayOpcodesResume = 6
+	GatewayOpcodesResume int = 6
 	// You should attempt to reconnect and resume immediately
-	GatewayOpcodesReconnect = 7
+	GatewayOpcodesReconnect int = 7
 	// Request information about offline guild members in a large guild
-	GatewayOpcodesRequestGuildMembers = 8
+	GatewayOpcodesRequestGuildMembers int = 8
 	// The session has been invalidated. You should reconnect and identify/resume accordingly
-	GatewayOpcodesInvalidSession = 9
+	GatewayOpcodesInvalidSession int = 9
 	// Sent immediately after connecting, contains the heartbeat_interval to use
-	GatewayOpcodesHello = 10
+	GatewayOpcodesHello int = 10
 	// 	Sent in response to receiving a heartbeat to acknowledge that it has been received
-	GatewayOpcodesHeartbeatACK = 11
+	GatewayOpcodesHeartbeatACK int = 11
 )
 
 //https://discord.com/developers/docs/topics/opcodes-and-status-codes#gateway-gateway-close-event-codes
-var (
+const (
 	// We're not sure what went wrong. Try reconnecting?
-	GatewayCloseCodesUnknownError = 4000
+	GatewayCloseCodesUnknownError int = 4000
 	// You sent an invalid Gateway opcode or an invalid payload for an opcode. Don't do that!
 	// See https://discord.com/developers/docs/topics/gateway#payloads-and-opcodes
-	GatewayCloseCodesUnknownOpcode = 4001
+	GatewayCloseCodesUnknownOpcode int = 4001
 	// You sent an invalid payload to us. Don't do that!
 	// See https://discord.com/developers/docs/topics/gateway#sending-payloads
-	GatewayCloseCodesDecodeError = 4002
+	GatewayCloseCodesDecodeError int = 4002
 	// You sent us a payload prior to identifying
 	// See https://discord.com/developers/docs/topics/gateway#identify
-	GatewayCloseCodesNotAuthenticated = 4003
+	GatewayCloseCodesNotAuthenticated int = 4003
 	// The account token sent with your identify payload is incorrect
 	// See https://discord.com/developers/docs/topics/gateway#identify
-	GatewayCloseCodesAuthenticationFailed = 4004
+	GatewayCloseCodesAuthenticationFailed int = 4004
 	// You sent more than one identify payload. Don't do that!
-	GatewayCloseCodesAlreadyAuthenticated = 4005
+	GatewayCloseCodesAlreadyAuthenticated int = 4005
 	// The sequence sent when resuming the session was invalid. Reconnect and start a new session
 	// See https://discord.com/developers/docs/topics/gateway#resume
-	GatewayCloseCodesInvalidSeq = 4007
+	GatewayCloseCodesInvalidSeq int = 4007
 	// Woah nelly! You're sending payloads to us too quickly. Slow it down! You will be disconnected on receiving this
-	GatewayCloseCodesRateLimited = 4008
+	GatewayCloseCodesRateLimited int = 4008
 	// Your session timed out. Reconnect and start a new one
-	GatewayCloseCodesSessionTimedOut = 4009
+	GatewayCloseCodesSessionTimedOut int = 4009
 	// You sent us an invalid shard when identifying
 	// See https://discord.com/developers/docs/topics/gateway#sharding
-	GatewayCloseCodesInvalidShard = 4010
+	GatewayCloseCodesInvalidShard int = 4010
 	// The session would have handled too many guilds - you are required to shard your connection in order to connect
 	// See https://discord.com/developers/docs/topics/gateway#sharding
-	GatewayCloseCodesShardingRequired = 4011
+	GatewayCloseCodesShardingRequired int = 4011
 	// You sent an invalid version for the gateway
-	GatewayCloseCodesInvalidAPIVersion = 4012
+	GatewayCloseCodesInvalidAPIVersion int = 4012
 	// You sent an invalid intent for a Gateway Intent. You may have incorrectly calculated the bitwise value
-	GatewayCloseCodesInvalidIntents = 4013
+	GatewayCloseCodesInvalidIntents int = 4013
 	// You sent a disallowed intent for a Gateway Intent. You may have tried to specify an intent that you have not
 	// enabled or are not whitelisted for
-	GatewayCloseCodesDisallowedIntents = 4014
+	GatewayCloseCodesDisallowedIntents int = 4014
 )
 
 // https://discord.com/developers/docs/topics/gateway#list-of-intents
-var (
-	Guilds                 = 1 << 0
-	GuildMembers           = 1 << 1
-	GuildBans              = 1 << 2
-	GuildEmojisAndStickers = 1 << 3
-	GuildIntegrations      = 1 << 4
-	GuildWebhooks          = 1 << 5
-	GuildInvites           = 1 << 6
-	GuildVoiceStates       = 1 << 7
-	GuildPresences         = 1 << 8
-	GuildMessages          = 1 << 9
-	GuildMessageReactions  = 1 << 10
-	GuildMessageTyping     = 1 << 11
-	DirectMessages         = 1 << 12
-	DirectMessageReactions = 1 << 13
-	DirectMessageTyping    = 1 << 14
-	MessageContent         = 1 << 15
-	GuildScheduledEvents   = 1 << 16
+const (
+	Guilds                 int = 1 << 0
+	GuildMembers           int = 1 << 1
+	GuildBans              int = 1 << 2
+	GuildEmojisAndStickers int = 1 << 3
+	GuildIntegrations      int = 1 << 4
+	GuildWebhooks          int = 1 << 5
+	GuildInvites           int = 1 << 6
+	GuildVoiceStates       int = 1 << 7
+	GuildPresences         int = 1 << 8
+	GuildMessages          int = 1 << 9
+	GuildMessageReactions  int = 1 << 10
+	GuildMessageTyping     int = 1 << 11
+	DirectMessages         int = 1 << 12
+	DirectMessageReactions int = 1 << 13
+	DirectMessageTyping    int = 1 << 14
+	MessageContent         int = 1 << 15
+	GuildScheduledEvents   int = 1 << 16
 )
 
 // https://discord.com/developers/docs/topics/gateway#commands-and-events-gateway-events
-var (
-	GatewayDispatchEventsApplicationCommandPermissionsUpdate = "APPLICATION_COMMAND_PERMISSIONS_UPDATE"
-	GatewayDispatchEventsChannelCreate                       = "CHANNEL_CREATE"
-	GatewayDispatchEventsChannelDelete                       = "CHANNEL_DELETE"
-	GatewayDispatchEventsChannelPinsUpdate                   = "CHANNEL_PINS_UPDATE"
-	GatewayDispatchEventsChannelUpdate                       = "CHANNEL_UPDATE"
-	GatewayDispatchEventsGuildBanAdd                         = "GUILD_BAN_ADD"
-	GatewayDispatchEventsGuildBanRemove                      = "GUILD_BAN_REMOVE"
-	GatewayDispatchEventsGuildCreate                         = "GUILD_CREATE"
-	GatewayDispatchEventsGuildDelete                         = "GUILD_DELETE"
-	GatewayDispatchEventsGuildEmojisUpdate                   = "GUILD_EMOJIS_UPDATE"
-	GatewayDispatchEventsGuildIntegrationsUpdate             = "GUILD_INTEGRATIONS_UPDATE"
-	GatewayDispatchEventsGuildMemberAdd                      = "GUILD_MEMBER_ADD"
-	GatewayDispatchEventsGuildMemberRemove                   = "GUILD_MEMBER_REMOVE"
-	GatewayDispatchEventsGuildMembersChunk                   = "GUILD_MEMBERS_CHUNK"
-	GatewayDispatchEventsGuildMemberUpdate                   = "GUILD_MEMBER_UPDATE"
-	GatewayDispatchEventsGuildRoleCreate                     = "GUILD_ROLE_CREATE"
-	GatewayDispatchEventsGuildRoleDelete                     = "GUILD_ROLE_DELETE"
-	GatewayDispatchEventsGuildRoleUpdate                     = "GUILD_ROLE_UPDATE"
-	GatewayDispatchEventsGuildStickersUpdate                 = "GUILD_STICKERS_UPDATE"
-	GatewayDispatchEventsGuildUpdate                         = "GUILD_UPDATE"
-	GatewayDispatchEventsIntegrationCreate                   = "INTEGRATION_CREATE"
-	GatewayDispatchEventsIntegrationDelete                   = "INTEGRATION_DELETE"
-	GatewayDispatchEventsIntegrationUpdate                   = "INTEGRATION_UPDATE"
-	GatewayDispatchEventsInteractionCreate                   = "INTERACTION_CREATE"
-	GatewayDispatchEventsInviteCreate                        = "INVITE_CREATE"
-	GatewayDispatchEventsInviteDelete                        = "INVITE_DELETE"
-	GatewayDispatchEventsMessageCreate                       = "MESSAGE_CREATE"
-	GatewayDispatchEventsMessageDelete                       = "MESSAGE_DELETE"
-	GatewayDispatchEventsMessageDeleteBulk                   = "MESSAGE_DELETE_BULK"
-	GatewayDispatchEventsMessageReactionAdd                  = "MESSAGE_REACTION_ADD"
-	GatewayDispatchEventsMessageReactionRemove               = "MESSAGE_REACTION_REMOVE"
-	GatewayDispatchEventsMessageReactionRemoveAll            = "MESSAGE_REACTION_REMOVE_ALL"
-	GatewayDispatchEventsMessageReactionRemoveEmoji          = "MESSAGE_REACTION_REMOVE_EMOJI"
-	GatewayDispatchEventsMessageUpdate                       = "MESSAGE_UPDATE"
-	GatewayDispatchEventsPresenceUpdate                      = "PRESENCE_UPDATE"
-	GatewayDispatchEventsStageInstanceCreate                 = "STAGE_INSTANCE_CREATE"
-	GatewayDispatchEventsStageInstanceDelete                 = "STAGE_INSTANCE_DELETE"
-	GatewayDispatchEventsStageInstanceUpdate                 = "STAGE_INSTANCE_UPDATE"
-	GatewayDispatchEventsReady                               = "READY"
-	GatewayDispatchEventsResumed                             = "RESUMED"
-	GatewayDispatchEventsThreadCreate                        = "THREAD_CREATE"
-	GatewayDispatchEventsThreadDelete                        = "THREAD_DELETE"
-	GatewayDispatchEventsThreadListSync                      = "THREAD_LIST_SYNC"
-	GatewayDispatchEventsThreadMembersUpdate                 = "THREAD_MEMBERS_UPDATE"
-	GatewayDispatchEventsThreadMemberUpdate                  = "THREAD_MEMBER_UPDATE"
-	GatewayDispatchEventsThreadUpdate                        = "THREAD_UPDATE"
-	GatewayDispatchEventsTypingStart                         = "TYPING_START"
-	GatewayDispatchEventsUserUpdate                          = "USER_UPDATE"
-	GatewayDispatchEventsVoiceServerUpdate                   = "VOICE_SERVER_UPDATE"
-	GatewayDispatchEventsVoiceStateUpdate                    = "VOICE_STATE_UPDATE"
-	GatewayDispatchEventsWebhooksUpdate                      = "WEBHOOKS_UPDATE"
-	GatewayDispatchEventsGuildScheduledEventCreate           = "GUILD_SCHEDULED_EVENT_CREATE"
-	GatewayDispatchEventsGuildScheduledEventUpdate           = "GUILD_SCHEDULED_EVENT_UPDATE"
-	GatewayDispatchEventsGuildScheduledEventDelete           = "GUILD_SCHEDULED_EVENT_DELETE"
-	GatewayDispatchEventsGuildScheduledEventUserAdd          = "GUILD_SCHEDULED_EVENT_USER_ADD"
-	GatewayDispatchEventsGuildScheduledEventUserRemove       = "GUILD_SCHEDULED_EVENT_USER_REMOVE"
+const (
+	GatewayDispatchEventsApplicationCommandPermissionsUpdate string = "APPLICATION_COMMAND_PERMISSIONS_UPDATE"
+	GatewayDispatchEventsChannelCreate                       string = "CHANNEL_CREATE"
+	GatewayDispatchEventsChannelDelete                       string = "CHANNEL_DELETE"
+	GatewayDispatchEventsChannelPinsUpdate                   string = "CHANNEL_PINS_UPDATE"
+	GatewayDispatchEventsChannelUpdate                       string = "CHANNEL_UPDATE"
+	GatewayDispatchEventsGuildBanAdd                         string = "GUILD_BAN_ADD"
+	GatewayDispatchEventsGuildBanRemove                      string = "GUILD_BAN_REMOVE"
+	GatewayDispatchEventsGuildCreate                         string = "GUILD_CREATE"
+	GatewayDispatchEventsGuildDelete                         string = "GUILD_DELETE"
+	GatewayDispatchEventsGuildEmojisUpdate                   string = "GUILD_EMOJIS_UPDATE"
+	GatewayDispatchEventsGuildIntegrationsUpdate             string = "GUILD_INTEGRATIONS_UPDATE"
+	GatewayDispatchEventsGuildMemberAdd                      string = "GUILD_MEMBER_ADD"
+	GatewayDispatchEventsGuildMemberRemove                   string = "GUILD_MEMBER_REMOVE"
+	GatewayDispatchEventsGuildMembersChunk                   string = "GUILD_MEMBERS_CHUNK"
+	GatewayDispatchEventsGuildMemberUpdate                   string = "GUILD_MEMBER_UPDATE"
+	GatewayDispatchEventsGuildRoleCreate                     string = "GUILD_ROLE_CREATE"
+	GatewayDispatchEventsGuildRoleDelete                     string = "GUILD_ROLE_DELETE"
+	GatewayDispatchEventsGuildRoleUpdate                     string = "GUILD_ROLE_UPDATE"
+	GatewayDispatchEventsGuildStickersUpdate                 string = "GUILD_STICKERS_UPDATE"
+	GatewayDispatchEventsGuildUpdate                         string = "GUILD_UPDATE"
+	GatewayDispatchEventsIntegrationCreate                   string = "INTEGRATION_CREATE"
+	GatewayDispatchEventsIntegrationDelete                   string = "INTEGRATION_DELETE"
+	GatewayDispatchEventsIntegrationUpdate                   string = "INTEGRATION_UPDATE"
+	GatewayDispatchEventsInteractionCreate                   string = "INTERACTION_CREATE"
+	GatewayDispatchEventsInviteCreate                        string = "INVITE_CREATE"
+	GatewayDispatchEventsInviteDelete                        string = "INVITE_DELETE"
+	GatewayDispatchEventsMessageCreate                       string = "MESSAGE_CREATE"
+	GatewayDispatchEventsMessageDelete                       string = "MESSAGE_DELETE"
+	GatewayDispatchEventsMessageDeleteBulk                   string = "MESSAGE_DELETE_BULK"
+	GatewayDispatchEventsMessageReactionAdd                  string = "MESSAGE_REACTION_ADD"
+	GatewayDispatchEventsMessageReactionRemove               string = "MESSAGE_REACTION_REMOVE"
+	GatewayDispatchEventsMessageReactionRemoveAll            string = "MESSAGE_REACTION_REMOVE_ALL"
+	GatewayDispatchEventsMessageReactionRemoveEmoji          string = "MESSAGE_REACTION_REMOVE_EMOJI"
+	GatewayDispatchEventsMessageUpdate                       string = "MESSAGE_UPDATE"
+	GatewayDispatchEventsPresenceUpdate                      string = "PRESENCE_UPDATE"
+	GatewayDispatchEventsStageInstanceCreate                 string = "STAGE_INSTANCE_CREATE"
+	GatewayDispatchEventsStageInstanceDelete                 string = "STAGE_INSTANCE_DELETE"
+	GatewayDispatchEventsStageInstanceUpdate                 string = "STAGE_INSTANCE_UPDATE"
+	GatewayDispatchEventsReady                               string = "READY"
+	GatewayDispatchEventsResumed                             string = "RESUMED"
+	GatewayDispatchEventsThreadCreate                        string = "THREAD_CREATE"
+	GatewayDispatchEventsThreadDelete                        string = "THREAD_DELETE"
+	GatewayDispatchEventsThreadListSync                      string = "THREAD_LIST_SYNC"
+	GatewayDispatchEventsThreadMembersUpdate                 string = "THREAD_MEMBERS_UPDATE"
+	GatewayDispatchEventsThreadMemberUpdate                  string = "THREAD_MEMBER_UPDATE"
+	GatewayDispatchEventsThreadUpdate                        string = "THREAD_UPDATE"
+	GatewayDispatchEventsTypingStart                         string = "TYPING_START"
+	GatewayDispatchEventsUserUpdate                          string = "USER_UPDATE"
+	GatewayDispatchEventsVoiceServerUpdate                   string = "VOICE_SERVER_UPDATE"
+	GatewayDispatchEventsVoiceStateUpdate                    string = "VOICE_STATE_UPDATE"
+	GatewayDispatchEventsWebhooksUpdate                      string = "WEBHOOKS_UPDATE"
+	GatewayDispatchEventsGuildScheduledEventCreate           string = "GUILD_SCHEDULED_EVENT_CREATE"
+	GatewayDispatchEventsGuildScheduledEventUpdate           string = "GUILD_SCHEDULED_EVENT_UPDATE"
+	GatewayDispatchEventsGuildScheduledEventDelete           string = "GUILD_SCHEDULED_EVENT_DELETE"
+	GatewayDispatchEventsGuildScheduledEventUserAdd          string = "GUILD_SCHEDULED_EVENT_USER_ADD"
+	GatewayDispatchEventsGuildScheduledEventUserRemove       string = "GUILD_SCHEDULED_EVENT_USER_REMOVE"
 )
 
 // Gateway Dispatch Payloads
