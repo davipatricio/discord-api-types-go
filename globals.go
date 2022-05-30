8c87f2bc2fc2cb397ad46f2dbb4947d02440e9f0
davipatricio <davipatricio2006@gmail.com>
2022-05-29 22:12:09 -0300

    feat: globals

---
+++ globals.go
@@ -0,0 +1,7 @@
+package globals
+
+// https://discord.com/developers/docs/reference#snowflakes
+type Snowflake string
+
+// https://discord.com/developers/docs/topics/permissions
+type Permissions string
