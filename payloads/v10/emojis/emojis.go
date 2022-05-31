3ed9d6b4713f17b3e62c2ac1be2274f214bdac6e
davipatricio <davipatricio2006@gmail.com>
2022-05-30 23:07:50 -0300

    payloads: add `omitempty` to nullable properties

--- payloads/v10/emojis/emojis.go
+++ payloads/v10/emojis/emojis.go
@@ -5,23 +5,23 @@ import "github.com/denkylabs/discord-api-types-go/globals"
 // Not documented but mentioned
 type APIPartialEmoji struct {
 	// Emoji id
-	Id globals.Snowflake `json:"id"`
+	Id globals.Snowflake `json:"id,omitempty"`
 	// Emoji name (can be null only in reaction emoji objects)
-	Name string `json:"name"`
+	Name string `json:"name,omitempty"`
 	// Whether this emoji is animated
-	Animated bool `json:"animated"`
+	Animated bool `json:"animated,omitempty"`
 }
 
 // https://discord.com/developers/docs/resources/emoji#emoji-object-emoji-structure
 type APIEmoji struct {
 	// Roles this emoji is whitelisted to
-	Roles []globals.Snowflake `json:"roles"`
+	Roles []globals.Snowflake `json:"roles,omitempty"`
 	// User that created this emoji
-	User interface{} `json:"user"` // TODO: APIUser
+	User interface{} `json:"user,omitempty"` // TODO: APIUser
 	// Whether this emoji must be wrapped in colons
-	RequiresColons bool `json:"requires_colons"`
+	RequiresColons bool `json:"requires_colons,omitempty"`
 	// Whether this emoji is managed
-	Managed bool `json:"managed"`
+	Managed bool `json:"managed,omitempty"`
 	// Whether this emoji can be used, may be false due to loss of Server Boosts
-	Available bool `json:"available"`
+	Available bool `json:"available,omitempty"`
 }
--- payloads/v10/guilds/guilds.go
+++ payloads/v10/guilds/guilds.go
@@ -80,52 +80,52 @@ type APIPartialGuild struct {
 	Splash string `json:"splash"`
 	// Banner hash
 	// See https://discord.com/developers/docs/reference#image-formatting
-	Banner string `json:"banner"`
+	Banner string `json:"banner,omitempty"`
 	// The description for the guild
-	Description string `json:"description"`
+	Description string `json:"description,omitempty"`
 	// Enabled guild features
 	// See https://discord.com/developers/docs/resources/guild#guild-object-guild-features
-	Features []interface{} `json:"features"` // TODO: Guild features
+	Features []interface{} `json:"features,omitempty"` // TODO: Guild features
 	// Verification level required for the guild
 	// See https://discord.com/developers/docs/resources/guild#guild-object-verification-level
-	VerificationLevel int `json:"verification_level"`
+	VerificationLevel int `json:"verification_level,omitempty"`
 	// The vanity url code for the guild
-	VanityURLCode string `json:"vanity_url_code"`
+	VanityURLCode string `json:"vanity_url_code,omitempty"`
 	// `true` if this guild is unavailable due to an outage
-	Unavailable bool `json:"unavailable"`
+	Unavailable bool `json:"unavailable,omitempty"`
 	// HACK: This should be the same as APIGuild
-	WelcomeScreen APIGuildWelcomeScreen `json:"welcome_screen"`
+	WelcomeScreen APIGuildWelcomeScreen `json:"welcome_screen,omitempty"`
 }
 
 // https://discord.com/developers/docs/resources/guild#guild-object-guild-structure
 type APIGuild struct {
 	// Icon hash, returned when in the template object
 	// See https://discord.com/developers/docs/reference#image-formatting
-	IconHash string `json:"icon_hash"`
+	IconHash string `json:"icon_hash,omitempty"`
 	// Discovery splash hash; only present for guilds with the "DISCOVERABLE" feature
 	// See https://discord.com/developers/docs/reference#image-formatting
-	DiscoverySplash string `json:"discovery_splash"`
+	DiscoverySplash string `json:"discovery_splash,omitempty"`
 	// `true` if the user is the owner of the guild
 	// This field is only received from https://discord.com/developers/docs/resources/user#get-current-user-guilds
-	Owner bool `json:"owner"`
+	Owner bool `json:"owner,omitempty"`
 	// ID of owner
 	OwnerID globals.Snowflake `json:"owner_id"`
 	// Total permissions for the user in the guild (excludes overrides)
 	// This field is only received from https://discord.com/developers/docs/resources/user#get-current-user-guilds
 	// See https://en.wikipedia.org/wiki/Bit_field
-	Permissions globals.Permissions `json:"permissions"`
+	Permissions globals.Permissions `json:"permissions,omitempty"`
 	// Voice region id for the guild
 	// See https://discord.com/developers/docs/resources/voice#voice-region-object
 	// @deprecated This field has been deprecated in favor of `rtc_region` on the channel.
 	Region string `json:"region"`
 	// ID of afk channel
-	AfkChannelId globals.Snowflake `json:"afk_channel_id"`
+	AfkChannelId globals.Snowflake `json:"afk_channel_id,omitempty"`
 	// AFK timeout in seconds
 	AfkTimeout int `json:"afk_timeout"`
 	// `true` if the guild widget is enabled
-	WidgetEnabled bool `json:"widget_enabled"`
+	WidgetEnabled bool `json:"widget_enabled,omitempty"`
 	// The channel id that the widget will generate an invite to, or `null` if set to no invite
-	WidgetChannelId globals.Snowflake `json:"widget_channel_id"`
+	WidgetChannelId globals.Snowflake `json:"widget_channel_id,omitempty"`
 	// Verification level required for the guild
 	// See https://discord.com/developers/docs/resources/guild#guild-object-verification-level
 	VerificationLevel int `json:"verification_level"`
@@ -140,12 +140,12 @@ type APIGuild struct {
 	Roles permissions.APIRole `json:"roles"`
 	// The welcome screen of a Community guild, shown to new members
 	// Returned in the invite object
-	WelcomeScreen APIGuildWelcomeScreen `json:"welcome_screen"`
+	WelcomeScreen APIGuildWelcomeScreen `json:"welcome_screen,omitempty"`
 }
 
 type APIGuildWelcomeScreen struct {
 	// The welcome screen short message
-	Description string `json:"description"`
+	Description string `json:"description,omitempty"`
 	// Array of suggested channels
 	WelcomeChannels []APIGuildWelcomeScreenChannel `json:"welcome_channels"`
 }
@@ -156,7 +156,7 @@ type APIGuildWelcomeScreenChannel struct {
 	/// The description shown for the channel
 	Description string `json:"description"`
 	// The emoji id of the emoji that is shown on the left of the channel
-	EmojiId globals.Snowflake `json:"emoji_id"`
+	EmojiId globals.Snowflake `json:"emoji_id,omitempty"`
 	// The emoji name of the emoji that is shown on the left of the channel
-	EmojiName string `json:"emoji_name"`
+	EmojiName string `json:"emoji_name,omitempty"`
 }
