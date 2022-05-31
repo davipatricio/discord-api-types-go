a095a931236bd5c7cab20ca64300f44ffc9e765e
davipatricio <davipatricio2006@gmail.com>
2022-05-30 22:43:49 -0300

    payloads add APIGuildWelcomeScreen

--- payloads/v10/guilds/guilds.go
+++ payloads/v10/guilds/guilds.go
@@ -69,38 +69,29 @@ type APIUnavailableGuild struct {
 type APIPartialGuild struct {
 	// Guild name (2-100 characters, excluding trailing and leading whitespace)
 	Name string `json:"name"`
-
 	// Icon hash
 	// See https://discord.com/developers/docs/reference#image-formatting
 	Icon string `json:"icon"`
-
 	// Splash hash
 	// See https://discord.com/developers/docs/reference#image-formatting
 	Splash string `json:"splash"`
-
 	// Banner hash
 	// See https://discord.com/developers/docs/reference#image-formatting
 	Banner string `json:"banner"`
-
 	// The description for the guild
 	Description string `json:"description"`
-
 	// Enabled guild features
 	// See https://discord.com/developers/docs/resources/guild#guild-object-guild-features
 	Features []interface{} `json:"features"` // TODO: Guild features
-
 	// Verification level required for the guild
 	// See https://discord.com/developers/docs/resources/guild#guild-object-verification-level
 	VerificationLevel int `json:"verification_level"`
-
 	// The vanity url code for the guild
 	VanityURLCode string `json:"vanity_url_code"`
-
 	// `true` if this guild is unavailable due to an outage
 	Unavailable bool `json:"unavailable"`
-
 	// HACK: This should be the same as APIGuild
-	WelcomeScreen interface{} `json:"welcome_screen"` // TODO: APIGuildWelcomeScreen
+	WelcomeScreen APIGuildWelcomeScreen `json:"welcome_screen"`
 }
 
 // https://discord.com/developers/docs/resources/guild#guild-object-guild-structure
@@ -108,41 +99,52 @@ type APIGuild struct {
 	// Icon hash, returned when in the template object
 	// See https://discord.com/developers/docs/reference#image-formatting
 	IconHash string `json:"icon_hash"`
-
 	// Discovery splash hash; only present for guilds with the "DISCOVERABLE" feature
 	// See https://discord.com/developers/docs/reference#image-formatting
 	DiscoverySplash string `json:"discovery_splash"`
-
 	// `true` if the user is the owner of the guild
 	// This field is only received from https://discord.com/developers/docs/resources/user#get-current-user-guilds
 	Owner bool `json:"owner"`
-
 	// ID of owner
 	OwnerID globals.Snowflake `json:"owner_id"`
-
 	// Total permissions for the user in the guild (excludes overrides)
 	// This field is only received from https://discord.com/developers/docs/resources/user#get-current-user-guilds
 	// See https://en.wikipedia.org/wiki/Bit_field
 	Permissions globals.Permissions `json:"permissions"`
-
 	// Voice region id for the guild
 	// See https://discord.com/developers/docs/resources/voice#voice-region-object
 	// @deprecated This field has been deprecated in favor of `rtc_region` on the channel.
 	Region string `json:"region"`
-
 	// ID of afk channel
 	AfkChannelId globals.Snowflake `json:"afk_channel_id"`
-
 	// AFK timeout in seconds
 	AfkTimeout int `json:"afk_timeout"`
-
 	// `true` if the guild widget is enabled
 	WidgetEnabled bool `json:"widget_enabled"`
-
 	// The channel id that the widget will generate an invite to, or `null` if set to no invite
 	WidgetChannelId globals.Snowflake `json:"widget_channel_id"`
-
 	// Verification level required for the guild
 	// See https://discord.com/developers/docs/resources/guild#guild-object-verification-level
 	VerificationLevel int `json:"verification_level"`
+	// The welcome screen of a Community guild, shown to new members
+	// Returned in the invite object
+	WelcomeScreen APIGuildWelcomeScreen `json:"welcome_screen"`
+}
+
+type APIGuildWelcomeScreen struct {
+	// The welcome screen short message
+	Description string `json:"description"`
+	// Array of suggested channels
+	WelcomeChannels []APIGuildWelcomeScreenChannel `json:"welcome_channels"`
+}
+
+type APIGuildWelcomeScreenChannel struct {
+	// The channel id that is suggested
+	ChannelId globals.Snowflake `json:"channel_id"`
+	/// The description shown for the channel
+	Description string `json:"description"`
+	// The emoji id of the emoji that is shown on the left of the channel
+	EmojiId globals.Snowflake `json:"emoji_id"`
+	// The emoji name of the emoji that is shown on the left of the channel
+	EmojiName string `json:"emoji_name"`
 }
