72de1b7a27d80e3ac14b1cfd713a6d04ede71087
davipatricio <davipatricio2006@gmail.com>
2022-06-01 13:53:44 -0300

    payloads/guilds: add GuildWidgetStyle constants

--- payloads/guilds/guilds.go
+++ payloads/guilds/guilds.go
@@ -195,6 +195,21 @@ const (
 	GuildHubTypeCollege
 )
 
+// https://discord.com/developers/docs/resources/guild#get-guild-widget-image-widget-style-options
+const (
+	// Shield style widget with Discord icon and guild members online count
+	GuildWidgetStyleShield  = "shield"
+	// Large image with guild icon, name and online count. "POWERED BY DISCORD" as the footer of the widget
+	GuildWidgetStyleBanner1 = "banner1"
+	// Smaller widget style with guild icon, name and online count. Split on the right with Discord logo
+	GuildWidgetStyleBanner2 = "banner2"
+	// Large image with guild icon, name and online count. In the footer, Discord logo on the left and "Chat Now" on the right
+	GuildWidgetStyleBanner3 = "banner3"
+	// Large Discord logo at the top of the widget. Guild icon, name and online count in the middle portion of the widget
+	// and a "JOIN MY SERVER" button at the bottom
+	GuildWidgetStyleBanner4 = "banner4"
+)
+
 // https://discord.com/developers/docs/resources/guild#guild-object-system-channel-flags
 const (
 	// Suppress member join notifications
