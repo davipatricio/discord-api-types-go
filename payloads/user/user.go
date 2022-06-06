4103087ead5d9a9a222082d60887acb630a52b92
Davi Patricio <davipatricio@pm.me>
2022-06-06 18:29:57 -0300

    move APIConnection to separate package

--- payloads/user/user.go
+++ payloads/user/user.go
@@ -2,7 +2,6 @@ package user
 
 import (
 	"github.com/denkylabs/discord-api-types-go/globals"
-	"github.com/denkylabs/discord-api-types-go/payloads/guilds"
 )
 
 // https://discord.com/developers/docs/resources/user#user-object
@@ -89,41 +88,4 @@ const (
 	UserPremiumTypeNone UserPremiumType = iota
 	UserPremiumTypeNitroClassic
 	UserPremiumTypeNitro
-)
-
-// https://discord.com/developers/docs/resources/user#connection-object
-type APIConnection struct {
-	// ID of the connection account
-	Id globals.Snowflake `json:"id"`
-	// The username of the connection account
-	Name string `json:"name"`
-	// The service of the connection
-	Type string `json:"type"`
-	// Whether the connection is revoked
-	Revoked bool `json:"revoked"`
-	// An array of partial server integrations
-	// See https://discord.com/developers/docs/resources/guild#integration-object
-	Integrations guilds.APIGuildIntegration `json:"integrations"`
-	// Whether the connection is verified
-	Verified bool `json:"verified"`
-	// Whether friend sync is enabled for this connection
-	FriendSync bool `json:"friend_sync"`
-	// Whether activities related to this connection will be shown in presence updates
-	ShowActivity bool `json:"show_activity"`
-	// Visibility of this connection
-	// See https://discord.com/developers/docs/resources/user#connection-object-visibility
-	Visibility ConnectionVisibility `json:"visibility"`
-}
-
-// Visibility of this connection
-// See https://discord.com/developers/docs/resources/user#connection-object-visibility
-type ConnectionVisibility uint16
-
-// Visibility of this connection
-// See https://discord.com/developers/docs/resources/user#connection-object-visibility
-const (
-	// Invisible to everyone except the user themselves
-	ConnectionVisibilityNone ConnectionVisibility = iota
-	// Visible to everyone
-	ConnectionVisibilityEveryone
 )
