db78efebc294b9438fa3059a3af3d53179b6c583
Davi Patricio <davipatricio@pm.me>
2022-06-09 19:57:24 -0300

    rest/errors: add error 20024

--- rest/errors.go
+++ rest/errors.go
@@ -73,6 +73,8 @@ const (
 
 	RESTJSONErrorCodesAnnouncementEditLimitExceeded RESTJSONErrorCodes = 20022
 
+	UnderMinimumAge RESTJSONErrorCodes = 20024
+
 	RESTJSONErrorCodesChannelSendRateLimit RESTJSONErrorCodes = 20028
 	RESTJSONErrorCodesServerSendRateLimit  RESTJSONErrorCodes = 20029
 
