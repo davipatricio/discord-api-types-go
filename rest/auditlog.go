56c8e252d1689e94dca290f8d11c1832c5d367f3
Davi Patricio <davipatricio@pm.me>
2022-06-09 20:31:49 -0300

    payloads: add auditlog types

--- rest/auditlog.go
+++ rest/auditlog.go
@@ -1,17 +1,20 @@
 package rest
 
-import "github.com/denkylabs/discord-api-types-go/globals"
+import (
+	"github.com/denkylabs/discord-api-types-go/globals"
+	"github.com/denkylabs/discord-api-types-go/payloads"
+)
 
 // https://discord.com/developers/docs/resources/audit-log#get-guild-audit-log
 type RESTGetAPIAuditLogQuery struct {
 	// Filter the log for actions made by a user
 	UserId globals.Snowflake `json:"user_id"`
 	// The type of audit log events
-	ActionType interface{} `json:"action_type"` // TODO: AuditLogEvent
+	ActionType payloads.AuditLogEvent `json:"action_type"`
 	// Filter the log before a certain entry ID
 	Before globals.Snowflake `json:"before"`
 	//  How many entries are returned (default 50, minimum 1, maximum 100). Defaults to 50
 	Limit uint8 `json:"limit"`
 }
 
-type RESTGetAPIAuditLogResult interface{} // TODO: APIAuditLog
+type RESTGetAPIAuditLogResult payloads.APIAuditLog
